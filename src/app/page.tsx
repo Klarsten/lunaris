110a70e3eab08095028be9ceff0d593b281b7e68
klarst <62829604+klarst@users.noreply.github.com>
2025-03-14 10:25:48 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,13 +29,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Localization{" "}
+            About{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Connect to native culture.
+            Lunaris Global Limited
+            RM A1-13,BLK A 3/F,YEE LIM IND CTR, 2-28 KWAI LOK ST, KWAI CHUNG, Hongkong
           </p>
         </a>
 
